7d41d47fe2966932eea4c56222567d80040f0951
Sameeh Hussein <sameehhussein02@gmail.com>
2023-12-20 23:09:40 +0200

    Add RestaurantReservationDbContext and All DbSets

---
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RestaurantReservation.Db.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReservation.Db
+{
+    public class RestaurantReservationDbContext: DbContext
+    {
+        public DbSet<Restaurant> restaurants {  get; set; }
+        public DbSet<Reservation> reservations { get; set;}
+        public DbSet<Customer> customers { get; set; }
+        public DbSet<Employee> employees { get; set; }
+        public DbSet<Order> orders { get; set; }
+        public DbSet<Table> tables { get; set; }
+        public DbSet<MenuItems> MenuItems { get; set; }
+        public DbSet<OrderItems> orderItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("AppSetting.json")
+                .Build();
+
+            var connectionString = configuration.GetSection("constr").Value;
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Order>()
+                .Ignore(o => o.totalAmount);
+        }
+    }
+}
--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -7,11 +7,7 @@ namespace RestaurantReservation
     {
         static void Main(string[] args)
         {
-            var configuration = new ConfigurationBuilder()
-                .AddJsonFile("AppSetting.json")
-                .Build();
 
-            var connectionString = configuration.GetSection("constr").Value;
         }
     }
 }
