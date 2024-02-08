d2354f5ff585de6cda6957d8a3b1db687500ac03
Sameeh Hussein <sameehhussein02@gmail.com>
2024-02-08 02:59:21 +0200

    Improved Restaurant API Logging

--- RestaurantReservation.API/Controllers/RestaurantController.cs
+++ RestaurantReservation.API/Controllers/RestaurantController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Castle.Core.Resource;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -17,31 +18,50 @@ namespace RestaurantReservation.API.Controllers
     {
         private readonly IRestaurantService _restaurantService;
         private readonly IMapper _mapper;
+        private readonly ILogger<RestaurantController> _logger;
 
-        public RestaurantController(IRestaurantService restaurantService, IMapper mapper)
+        public RestaurantController(
+            IRestaurantService restaurantService, 
+            IMapper mapper,
+            ILogger<RestaurantController> logger)
         {
-            _restaurantService = restaurantService ?? throw new ArgumentNullException(nameof(restaurantService));
-            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _restaurantService = restaurantService ?? 
+                throw new ArgumentNullException(nameof(restaurantService));
+            _mapper = mapper ?? 
+                throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? 
+                throw new ArgumentNullException(nameof(logger));
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<RestaurantDTO>>> GetAllRestaurants()
         {
+            _logger.LogInformation("GetAllRestaurants Start Getting all restaurants");
+
             var restaurants = await _restaurantService.GetAllAsync();
             var result = _mapper.Map<IEnumerable<RestaurantDTO>>(restaurants);
+
+            _logger.LogInformation($"GetAllRestaurants retrieved {restaurants.Count()} restaurants successfully.");
+
             return Ok(result);
         }
 
         [HttpGet("{restaurantid}", Name = "GetRestaurantById")]
         public async Task<ActionResult<CustomerDTO>> GetRestaurantById(int restaurantid)
         {
+            _logger.LogInformation($"GetRestaurantById started for restaurant with ID: {restaurantid}.");
+
             var restaurant = await _restaurantService.GetByIdAsync(restaurantid);
             if (restaurant == null)
             {
+                _logger.LogWarning($"Restaurant with ID {restaurantid} not found.");
                 return NotFound();
             }
 
             var result = _mapper.Map<RestaurantDTO>(restaurant);
+
+            _logger.LogInformation($"Retrieved restaurant with ID: {restaurantid} successfully.");
+
             return Ok(result);
         }
 
@@ -49,19 +69,31 @@ namespace RestaurantReservation.API.Controllers
         [Authorize(Roles = Roles.Admin)]
         public async Task<ActionResult> AddRestaurant(RestaurantForCreationDTO restaurantForCreationDTO)
         {
-            var restaurant = _mapper.Map<Restaurant>(restaurantForCreationDTO);
+            _logger.LogInformation("AddRestaurant attempting to add a new restaurant.");
 
-            await _restaurantService.CreateAsync(restaurant);
+            try
+            {
+                var restaurant = _mapper.Map<Restaurant>(restaurantForCreationDTO);
 
-            var restaurantToReturn = _mapper.Map<RestaurantDTO>(restaurant);
+                await _restaurantService.CreateAsync(restaurant);
 
-            return CreatedAtRoute("GetRestaurantById",
-                    new
-                    {
-                        restaurantid = restaurantToReturn.RestaurantId
-                    },
-                    restaurantToReturn
-                );
+                var restaurantToReturn = _mapper.Map<RestaurantDTO>(restaurant);
+
+                _logger.LogInformation($"New Restaurant with ID: {restaurantToReturn.RestaurantId} added successfully.");
+
+                return CreatedAtRoute("GetRestaurantById",
+                        new
+                        {
+                            restaurantid = restaurantToReturn.RestaurantId
+                        },
+                        restaurantToReturn
+                    );
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while adding a new restaurant.");
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
         }
 
         [HttpPut("{restaurantid}")]
@@ -71,32 +103,57 @@ namespace RestaurantReservation.API.Controllers
             RestaurantForUpdateDTO restaurantForUpdateDTO
             )
         {
-            var restaurant = await _restaurantService.GetByIdAsync(restaurantid);
-            if (restaurant == null)
+            _logger.LogInformation($"UpdateRestaurant attempting to update restaurant with ID {restaurantid}.");
+
+            try
             {
-                return NotFound();
+                var restaurant = await _restaurantService.GetByIdAsync(restaurantid);
+                if (restaurant == null)
+                {
+                    _logger.LogWarning($"Restaurant with ID {restaurantid} not found.");
+                    return NotFound();
+                }
+
+                _mapper.Map(restaurantForUpdateDTO, restaurant);
+                await _restaurantService.UpdateAsync(restaurant);
+
+                _logger.LogInformation($"Restaurant with ID {restaurantid} updated successfully.");
+
+                return NoContent();
             }
-
-            _mapper.Map(restaurantForUpdateDTO, restaurant);
-
-            await _restaurantService.UpdateAsync(restaurant);
-
-            return NoContent();
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, $"An error occurred while updating restaurant with ID {restaurantid}.");
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
         }
 
         [HttpDelete("{restaurantid}")]
         [Authorize(Roles = Roles.Admin)]
-        public async Task<ActionResult> DeleteCustomer(int restaurantid)
+        public async Task<ActionResult> DeleteRestaurant(int restaurantid)
         {
-            var resturant = await _restaurantService.GetByIdAsync(restaurantid);
-            if (resturant == null)
+            _logger.LogInformation($"DeleteRestaurant attempting to delete restaurant with ID {restaurantid}.");
+
+            try
             {
-                return NotFound();
+                var resturant = await _restaurantService.GetByIdAsync(restaurantid);
+                if (resturant == null)
+                {
+                    _logger.LogWarning($"Restaurant with ID {restaurantid} not found. Unable to delete.");
+                    return NotFound();
+                }
+
+                await _restaurantService.DeleteAsync(resturant);
+
+                _logger.LogInformation($"Restaurant with ID {restaurantid} deleted successfully.");
+
+                return NoContent();
             }
-
-            await _restaurantService.DeleteAsync(resturant);
-
-            return NoContent();
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"An error occurred while deleting restaurant with ID {restaurantid}.");
+                return StatusCode(500, "An error occurred while processing your request.");
+            }
         }
     }
 }
